d35c6765e7598f4599a605b4132cd190791711f1
student <yhkim5719@csu.fullerton.edu>
2018-09-24 19:56:04 -0700

    for vector range loop practice

--- rangeLoop.cpp
+++ rangeLoop.cpp
@@ -8,9 +8,20 @@ int main() {
 
   for (double h : height) {
     cout << "Height?";
-    cin >> h; // "h" is not a vector element, but a vector adress.
+    cin >> h; // "h" is not a vector element, but a vector address.
     h = h - 2.0;
     cout << h << ", " << height[1] << endl; // height[1] = 0, still
+    averageHeight += h;
+  }
+
+  averageHeight = averageHeight / height.size();
+  cout << averageHeight << ", " << height[0] << ", " << height[1] << ", " << height[2] << endl;
+
+    for (double &h : height) {
+    cout << "Height?";
+    cin >> h; // Now, "h" is a vector element, not a vector address.
+    h = h - 2.0;
+    cout << h << ", " << height[1] << endl; // height[1] == h, now.
     averageHeight += h;
   }
 
